ca4a7fa324cbeb9fbadeb08e9707d5005cf3c42e
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-05-13 23:45:55 +0530

    started navbar

---
+++ src/Components/NavbarBanner/navbarBanner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './navbarBanner.css'
+const NavbarBanner = () => {
+  return (
+    <div>
+      NavebarBanner 
+    </div>
+  )
+}
+
+export default NavbarBanner;
---
+++ src/Components/NavbarBelt/navbarBelt.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbarBelt = () => {
+  return (
+    <div>
+      belt of navbar
+    </div>
+  )
+}
+
+export default navbarBelt
