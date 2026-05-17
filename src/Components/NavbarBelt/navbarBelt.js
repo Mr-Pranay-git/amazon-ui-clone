5989c6fbc41e4913024d415bdb14bd6615a65e85
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-05-17 23:44:25 +0530

    navbar left logo completed

--- src/Components/NavbarBelt/navbarBelt.js
+++ src/Components/NavbarBelt/navbarBelt.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './navbarBelt.css'
+import amazonLogo from '../../Assets/amazonLogo.png'
 
 const navbarBelt = () => {
   return (
-    <div>
-      belt of navbar
+    <div className='navbarBelt'>
+      <div className='leftNavBelt'>
+        <div className="leftNavBeltLogo">
+          <img className='amazonLogoNavbar' src={amazonLogo} alt='amazonLogo' />
+          <span className='navbar_inLogo'>.in</span>
+        </div>
+        <div className='navbarBeltLocation'>
+          <div className='navbarBeltLocationImg'>
+              
+          </div>
+        </div>
+      </div>
+      <div className='navbarBeltSearchBox'>
+        
+      </div>
+      <div className='rightSideNavbarBelt'>
+
+      </div>
     </div>
   )
 }
