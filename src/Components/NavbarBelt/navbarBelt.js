579ddb21f220275d28993adb85bce4ceeba27306
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-05-21 00:00:19 +0530

    location is added in navbar belt

--- src/Components/NavbarBelt/navbarBelt.js
+++ src/Components/NavbarBelt/navbarBelt.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import './navbarBelt.css'
-import amazonLogo from '../../Assets/amazonLogo.png'
+import React from 'react';
+import './navbarBelt.css';
+import amazonLogo from '../../Assets/amazonLogo.png';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const navbarBelt = () => {
   return (
@@ -12,16 +14,25 @@ const navbarBelt = () => {
         </div>
         <div className='navbarBeltLocation'>
           <div className='navbarBeltLocationImg'>
-              
+              <LocationOnOutlinedIcon className='navbarBeltLocationImgIcon' sx={{fontSize: "22px"}} />
+          </div>
+          <div className='navbarBeltLocationPlace'>
+            <div className='navbarBeltLocationTop'>Delivering to Pune 411032</div>
+            <div className='navbarBeltLocationBottom'>Update Location</div>
           </div>
         </div>
       </div>
       <div className='navbarBeltSearchBox'>
-        
+        <div className='navbarBeltSearchDiv'>
+          <div> All</div>
+          <div className='arrow'>
+            <ArrowDropDownIcon/>
+          </div>
+        </div>
       </div>
       <div className='rightSideNavbarBelt'>
 
-      </div>
+      </div>  
     </div>
   )
 }
