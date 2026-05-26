afee77704e292c64fe0656a81c66093fb5ffcad8
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-05-26 23:59:29 +0530

    nav banner need improvement

--- src/Components/NavbarBanner/navbarBanner.js
+++ src/Components/NavbarBanner/navbarBanner.js
@@ -2,14 +2,35 @@ import React from 'react'
 import './navbarBanner.css'
 import MenuIcon from '@mui/icons-material/Menu';
 const NavbarBanner = () => {
+  const options = [
+    { "name": "Fresh" },
+    { "name": "Amazon miniT" },
+    { "name": "Sell" },
+    { "name": "Best Sellers" },
+    { "name": "Todays Deals" },
+    { "name": "Mobiles" },
+    { "name": "Electronics" },
+    { "name": "Prime" },
+    { "name": "Customer Service" },
+    { "name": "Fashion" },
+    { "name": "Home & Kitchen" }
+  ]
   return (
     <div className='navbarBanner'>
       <div className='navbarBannerOptionsLeft'>
-        
+
         <div className='optionsNavbarBanner'>
-          <MenuIcon sx={{fontSize: '24px'}}/>
+          <MenuIcon sx={{ fontSize: '24px' }} />
           <div className='allOptionsNavbarBanner'>All</div>
         </div>
+        {
+        options.map((elem, index) => {
+          return (<div className='optionsNavbarBanner' key={index}>
+            <div className='allOptionsNavbarBanner'>{elem.name}</div>
+          </div>)
+        })
+        }
+
 
       </div>
       <div className='navbarBannerRightside'>
