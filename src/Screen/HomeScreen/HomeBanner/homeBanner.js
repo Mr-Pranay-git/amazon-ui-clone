8818450b5e87b0495135fbc595fc9ed69f41b1c5
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-06-02 16:35:17 +0530

    home page images added

--- src/Screen/HomeScreen/HomeBanner/homeBanner.js
+++ src/Screen/HomeScreen/HomeBanner/homeBanner.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import './homeBanner.css'
+import homeBannerItemProduct from '../../../homeProduct.json'
 const HomeBanner = () => {
   return (
     <div className='homeBanner'>
-        <img className='homeBannerimg' src='https://m.media-amazon.com/images/I/61Dr+oVuClL._SX3000_.jpg' />
-        <div className="grayBackgroundHomeBanner"></div>
+      <img className='homeBannerimg' src='https://m.media-amazon.com/images/I/61Dr+oVuClL._SX3000_.jpg' />
+      <div className="grayBackgroundHomeBanner"></div>
 
-        <div className='homeBannerItemDiv'>
+      <div className='homeBannerItemDiv'>
 
-        </div>
+        {
+          homeBannerItemProduct.product.map((item, index) => {
+
+            return (
+              <div className="homeBannerItemDivCard">
+                <div className="homeBannerItemDivCardTitle">{item.itemTitle}</div>
+                <div className="imgHomeBannerItemDivCard">
+                  {
+                    item.imgs.map((it, ind) => {
+                      return (
+                        <div className="imgBannerHomeDiv">
+                          <img className="imgBannerHomeDivImg" src={it} alt='product image' />
+                          <div className="imgBannerImgName">Cushion cover, bedsheet & more </div>
+                        </div>
+                      )
+                    })
+                  }
+                  
+                </div>
+
+                <div className='seeMoreHomeBanner'>See More</div>
+              </div>
+            )
+          })
+        }
+
+      </div>
     </div>
   )
 }
