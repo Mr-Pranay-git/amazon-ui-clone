e72f76348ed6ce21421546840335da5fb012be23
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-06-12 23:05:34 +0530

    home page completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/navbar'
 import HomeScreen from './Screen/HomeScreen/homeScreen';
+import Footer from './Components/Footer/footer';
 
 function App() {
   return (
     <div className='App'> 
       <Navbar/>
       <HomeScreen/>
+      <Footer/>
     </div>
   );
 }
