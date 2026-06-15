94bb6fb1612d35f3b6954fc0015ec555008761b3
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-06-16 00:00:08 +0530

    nav bar of products

--- src/Screen/HomeScreen/Products/products.js
+++ src/Screen/HomeScreen/Products/products.js
@@ -8,7 +8,15 @@ const Products = () => {
         <div className='productTopBannerItems'>
           Electronics
         </div>
-        <div className=''></div>
+        <div className='productTopBannerItemsSubMenu'>Mobiles & Accessories</div>
+        <div className='productTopBannerItemsSubMenu'>Laptops & Accessories</div>
+        <div className='productTopBannerItemsSubMenu'>TV & Home Entertainment</div>
+        <div className='productTopBannerItemsSubMenu'>Audio</div>
+        <div className='productTopBannerItemsSubMenu'>Cameras</div>
+        <div className='productTopBannerItemsSubMenu'>Computer Penpherals</div>
+        <div className='productTopBannerItemsSubMenu'>Smart Technology</div>
+        <div className='productTopBannerItemsSubMenu'>Musical Instruments</div>
+        <div className='productTopBannerItemsSubMenu'>Office & Stationary</div>
       </div>
     </div>
   )
