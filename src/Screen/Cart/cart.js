7b587831bcc6e561f17c5132870c0921f5696a79
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-07-26 01:57:59 +0530

    working on cart added products

--- src/Screen/Cart/cart.js
+++ src/Screen/Cart/cart.js
@@ -7,8 +7,19 @@ const Cart = () => {
         <div className='topLeftCart'>
             <div className='topLeftCartTitle'>Shopping Cart</div>
             <div className='desellectAllCart'>Deselect all items</div>
+            <div className='cartPriceTextDivider'>Price</div>
 
-            
+            <div className='cartItemsDiv'>
+              <div className='cartItemBlock'>
+                <div className='cartItemLeftBlock'>
+                  <div className='cartItemLeftBlockImage'>
+                    <img className='cartItemLeftBlockImg' src='https://m.media-amazon.com/images/I/51QXIJUo+lL._AC_UF480,480_SR480,480_.jpg' />
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
         </div>
 
         <div className='topRightCart'>
