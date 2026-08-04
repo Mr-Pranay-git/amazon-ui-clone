b5d44dcd972201a19df954b35aaac29eeaa4fc02
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-08-04 23:59:40 +0530

    remove from catr case added

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,3 +1,20 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_CART_QUANTITY, REMOVE_FROM_CART, CLEAR } from "./actionsType";
 
-    
+const initialState = {
+    items:[]
+};
+
+const cartReducer = (state = initialState, action)=>{
+    switch(action.type){
+        case ADD_TO_CART:
+            return{
+                ...state,
+                items: [...state.items, action.payload]
+            };
+            case REMOVE_FROM_CART:
+                return{
+                    ...state,
+                    items: state.items.filter(item => item.id !== action.payload)
+                };
+    }
+}
