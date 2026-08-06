9da02ff72846c252f7e63358ced0ade5c63a8c54
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-08-06 23:55:51 +0530

    refactor: update cart reducer to include CLEAR_CART action and clean up imports

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,20 +1,34 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_CART_QUANTITY, REMOVE_FROM_CART, CLEAR } from "./actionsType";
+import { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_CART_QUANTITY, REMOVE_FROM_CART, CLEAR, CLEAR_CART } from "./actionsType";
 
 const initialState = {
-    items:[]
+    items: []
 };
 
-const cartReducer = (state = initialState, action)=>{
-    switch(action.type){
+const cartReducer = (state = initialState, action) => {
+    switch (action.type) {
         case ADD_TO_CART:
-            return{
+            return {
                 ...state,
                 items: [...state.items, action.payload]
             };
-            case REMOVE_FROM_CART:
-                return{
-                    ...state,
-                    items: state.items.filter(item => item.id !== action.payload)
-                };
+        case REMOVE_FROM_CART:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            };
+        case UPDATE_CART_QUANTITY:
+            return{
+                ...state,
+                items: state.items.map(item =>
+                    item.id === action.payload.itemId ? {...item, quantity:  action.payload.quantity} : item
+                )
+            };
+        case CLEAR_CART:
+            return {
+                ...state,
+                items:[]
+            }
+        default:
+                
     }
 }
